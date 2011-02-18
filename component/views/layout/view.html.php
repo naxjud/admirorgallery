6bccedd6c199fb50c6fcf46b122f2813ef3e4732
nikola83 <nikola83@87552490-4604-c2f1-3d08-01d065ce2e82>
2011-02-18 21:42:09 +0000

    Sql query corrected for saving parametars, version numbers updated

--- component/views/layout/view.html.php
+++ component/views/layout/view.html.php
@@ -4,9 +4,10 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
  
 jimport( 'joomla.application.component.view');
- 
+
 class AdmirorgalleryViewLayout extends JView
 {
+
     function display($tpl = null)
     {
 
