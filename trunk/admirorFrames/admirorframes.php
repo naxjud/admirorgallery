3a183df54fbd79bcb30cd293bd84dcc3bb85c334
Nikola83@gmail.com <Nikola83@gmail.com@87552490-4604-c2f1-3d08-01d065ce2e82>
2011-07-24 22:51:36 +0000

    Small issues fixe. Url encode added for passing path using querystring, added default colorixer color to fix notices Dates corrected

--- trunk/admirorFrames/admirorframes.php
+++ trunk/admirorFrames/admirorframes.php
@@ -1,13 +1,12 @@
 <?php
 /*
 // Admiror Frames
-// Author: Igor Kekeljevic & Nikola Vasiljevski, 2010.
+// Author: Igor Kekeljevic & Nikola Vasiljevski, 2011.
 */
-
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
-// Import library dependencies
+//Import library dependencies
 jimport('joomla.event.plugin');
 jimport('joomla.plugin.plugin');
 jimport( 'joomla.filesystem.folder' );
@@ -35,7 +34,7 @@ class plgContentAdmirorframes extends JPlugin
   // Default parameters
 	$AF->staticParams['template'] = $this->params->get('af_template', 'default');
 	$AF->staticParams['bgcolor'] = $this->params->get('af_bgcolor', 'white');	
-	$AF->staticParams['colorize'] = $this->params->get('af_colorize', '');	
+	$AF->staticParams['colorize'] = $this->params->get('af_colorize', '000000');	
 	$AF->staticParams['ratio'] = $this->params->get('af_ratio', '100');
 	$AF->staticParams['width'] = $this->params->get('af_width', '100%');
 	$AF->staticParams['height'] = $this->params->get('af_height', '');	
@@ -49,31 +48,28 @@ class plgContentAdmirorframes extends JPlugin
     $AF->params['templates_ROOT'] = JURI::root().'plugins/content/admirorframes/templates/';  
 
 		
-  if (preg_match_all("#{AF[^}]*}(.*?){/AF}#s", $row->text, $matches, PREG_PATTERN_ORDER)>0)
-  {	
-    
-    foreach($matches[0] as $matchKey => $matchValue)
-    {		
-    
-        $html=$AF->AF_createFrame(preg_replace("/{.+?}/", "", $matchValue), $matchValue);
-        $row->text = str_replace( $matchValue, $html , $row->text);
-      
-    }
-    
-    $row->text .='<div style="clear:both"></div>';
-    
-    /* ========================= SIGNATURE ====================== */
-    if($af_showSignature==1){
-            $row->text .= '<div style="display:block; font-size:10px;">';
-    }else{
-            $row->text .= '<div style="display:block; font-size:10px; overflow:hidden; height:1px; padding-top:1px;">';
-    }
-    $row->text .= '<br /><a href="http://www.admiror-design-studio.com/admiror/en/design-resources/joomla-admiror-frames" target="_blank">AdmirorFrames 1.7.x</a>, '.JText::_("author/s").' <a href="http://www.vasiljevski.com/" target="_blank">Vasiljevski</a> & <a href="http://www.admiror-design-studio.com" target="_blank">Kekeljevic</a>.<br /></div>';
-  }//if (preg_match_all("#{AdmirorFrames[^}]*}(.*?){/AdmirorFrames}#s", $row->text, $matches, PREG_PATTERN_ORDER)>0)
-	
+	  if (preg_match_all("#{AF[^}]*}(.*?){/AF}#s", $row->text, $matches, PREG_PATTERN_ORDER)>0)
+	  {	
+		
+		foreach($matches[0] as $matchKey => $matchValue)
+		{		
+		
+			$html=$AF->AF_createFrame(preg_replace("/{.+?}/", "", $matchValue), $matchValue);
+			$row->text = str_replace( $matchValue, $html , $row->text);
+		  
+		}
+		
+		$row->text .='<div style="clear:both"></div>';
+		
+		/* ========================= SIGNATURE ====================== */
+		if($AF->staticParams['showSignature']==1){
+				$row->text .= '<div style="display:block; font-size:10px;">';
+		}else{
+				$row->text .= '<div style="display:block; font-size:10px; overflow:hidden; height:1px; padding-top:1px;">';
+		}
+		$row->text .= '<br /><a href="http://www.admiror-design-studio.com/admiror/en/design-resources/joomla-admiror-frames" target="_blank">AdmirorFrames 1.7.x</a>, '.JText::_("author/s").' <a href="http://www.vasiljevski.com/" target="_blank">Vasiljevski</a> & <a href="http://www.admiror-design-studio.com" target="_blank">Kekeljevic</a>.<br /></div>';
+	  }//if (preg_match_all("#{AdmirorFrames[^}]*}(.*?){/AdmirorFrames}#s", $row->text, $matches, PREG_PATTERN_ORDER)>0)
 
-  
-	
   }//function onPrepareContent(&$row, &$params, $limitstart) {
 
 }//class plgContentAdmirorFrames extends JPlugin
