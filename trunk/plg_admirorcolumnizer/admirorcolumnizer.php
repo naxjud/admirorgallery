3c40d0c65ff887d1c523983158d83da0cee5ab42
nikola83 <nikola83@87552490-4604-c2f1-3d08-01d065ce2e82>
2011-10-14 09:52:25 +0000

    File now supports Joomla 1.5 and greater versions (1.6 and 1.7)

--- trunk/plg_admirorcolumnizer/admirorcolumnizer.php
+++ trunk/plg_admirorcolumnizer/admirorcolumnizer.php
@@ -20,38 +20,56 @@ class plgContentAdmirorcolumnizer extends JPlugin {
     //Constructor
     function plgContentAdmirorcolumnizer(&$subject) {
         parent::__construct($subject);
-        // load plugin parameters
+        // jimport needed by Joomla 1.6 and >
+		jimport('joomla.html.parameter');
         $this->plugin = JPluginHelper::getPlugin('content', 'admirorcolumnizer');
         $this->params = new JParameter($this->plugin->params);
     }
-    function onPrepareContent(&$row, &$params, $limitstart) {
-
-	if ( !preg_match("#{AC[^}]*}(.*?){/AC}|{ac[^}]*}(.*?){/ac}#s", strtoupper($row->text)) ) {
-		return;
-	}	
-	require_once (JPATH_BASE.DS.'plugins'.DS.'content'.DS.'admirorcolumnizer'.DS.'scripts'.DS.'AC_helper.php');
-
-	$AC = new AC_helper($this->params);  
-
-	$doc = &JFactory::getDocument();
-	$doc->addScript(JURI::root().'plugins/content/admirorcolumnizer/scripts/AC_jQuery.js');
-	$doc->addScript(JURI::root().'plugins/content/admirorcolumnizer/scripts/autocolumn.min.js');
-
-	if (preg_match_all("#{AC[^}]*}(.*?){/AC}|{ac[^}]*}(.*?){/ac}#s", $row->text, $matches, PREG_PATTERN_ORDER)>0)
-	{	
-		
-		$html="";
-		foreach($matches[0] as $matchKey => $matchValue)
-		{	
-
-			$html=$AC->AC_createColumns(preg_replace("/{.+?}/", "", $matchValue), $matchValue, $matchKey."_".rand(0,1000000), $doc->direction);
-			$row->text = str_replace( $matchValue, $html , $row->text);
-		  
+	//Joomla 1.5
+	public function onPrepareContent( &$row, &$params, $limitstart = 0 )
+	{
+		$row->text = $this->textToColumns($row->text);
+	}
+	//Joomla 1.6 and > function 
+	public function onContentPrepare($context, &$row, &$params, $page = 0)
+	{
+		if(is_object($row)) {
+			return $this->onPrepareContent($row, $params, $page);
+		} else {
+			$row = $this->textToColumns($row);
 		}
+		return true;
+	}
+	//This does all the work :)
+	private function textToColumns($text)
+	{
+		if ( !preg_match("#{AC[^}]*}(.*?){/AC}|{ac[^}]*}(.*?){/ac}#s", strtoupper(($text))) ) {
+			return;
+		}	
+		require_once (dirname(__FILE__).'/admirorcolumnizer/scripts/AC_helper.php');
 
-	}//if (preg_match_all("#{Admirorcolumnizer[^}]*}(.*?){/Admirorcolumnizer}#s", $row->text, $matches, PREG_PATTERN_ORDER)>0)
+		$AC = new AC_helper($this->params);  
 
-    }//onPrepareContent(&$row, &$params, $limitstart)
+		$doc = &JFactory::getDocument();
+		if(version_compare(JVERSION,'1.6.0') >= 0) {
+			$doc->addScript(JURI::root().'plugins/content/admirorcolumnizer/admirorcolumnizer/scripts/AC_jQuery.js');
+			$doc->addScript(JURI::root().'plugins/content/admirorcolumnizer/admirorcolumnizer/scripts/autocolumn.min.js');
+		} else {
+			$doc->addScript(JURI::root().'plugins/content/admirorcolumnizer/scripts/AC_jQuery.js');
+			$doc->addScript(JURI::root().'plugins/content/admirorcolumnizer/scripts/autocolumn.min.js');
+		}
+		
 
+		if (preg_match_all("#{AC[^}]*}(.*?){/AC}|{ac[^}]*}(.*?){/ac}#s", $text, $matches, PREG_PATTERN_ORDER)>0)
+		{	
+			$html="";
+			foreach($matches[0] as $matchKey => $matchValue)
+			{	
+				$html=$AC->AC_createColumns(preg_replace("/{.+?}/", "", $matchValue), $matchValue, $matchKey."_".rand(0,1000000), $doc->direction);
+				$text = str_replace( $matchValue, $html , $text);
+			}
+		}
+		return $text;
+	}
 }//class plgContentAdmirorcolumnizer extends JPlugin
 ?>
