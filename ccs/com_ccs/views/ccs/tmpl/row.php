baccb4813ae2a786c400766e481ddbdcee9d706e
Igor.Kekeljevic@gmail.com <Igor.Kekeljevic@gmail.com@87552490-4604-c2f1-3d08-01d065ce2e82>
2012-10-17 11:11:32 +0000

    Personal Notes loaded as external single php

--- ccs/com_ccs/views/ccs/tmpl/row.php
+++ ccs/com_ccs/views/ccs/tmpl/row.php
@@ -263,52 +263,7 @@ if($field_alias!="id"){
 </fieldset>
 
 <?php 
-// ############################################### PERSONAL NOTES
-
-echo '
-<hr />
-<div id="CCS_notes_widthSetter" style="padding:10px;">
-<fieldset>
-<label>
-'.JText::_("CCS_NOTES").'
-</label>
-<textarea spellcheck="false" style="display:block; resize: none !important; overflow:hidden;" id="ccs_notes">'.$_COOKIE["CCS_NOTES"].'</textarea>
-<div id="ccs_notes_wrap" style="white-space: pre-wrap; word-wrap: break-word; position:absolute; left:99999px"></div>
-'.JText::_("CCS_NOTES_DESC").'
-<br style="clear:both;" /><br style="clear:both;" />
-</fieldset>
-</div>
-';
-
-$CCS_NOTES_JS='
-
-function ccs_notes_update(){
-    var content = ""+$$("#ccs_notes").get("value");
-    $$("#ccs_notes_wrap").set("html",content.replace(/\n/g, "<br />")+"<br /><br />");
-    Cookie.write(\'CCS_NOTES\', content, {duration: 99999});
-    $$("#ccs_notes").setStyle("height",$$("#ccs_notes_wrap").getStyle("height"));
-}
-
-window.addEvent("domready", function(){
-
-    // Update widths
-    var css_nodes_width = $("CCS_notes_widthSetter").getComputedSize().width-20;
-    $$("#ccs_notes").setStyle("width",css_nodes_width+"px");
-    $$("#ccs_notes_wrap").setStyle("width",css_nodes_width+"px");
-
-    ccs_notes_update();
-
-    $$("#ccs_notes").addEvents({
-        "keyup": function(e){
-            ccs_notes_update();
-        }
-    });
-
-});
-';
-$this->doc->addScriptDeclaration($CCS_NOTES_JS);
-
-// ############################################### PERSONAL NOTES
+require_once('personal_notes.php');
 ?>
 
 
