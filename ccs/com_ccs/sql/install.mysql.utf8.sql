c5a3f7fd6f0f0ac8dad026772607550fad206401
Nikola83@gmail.com <Nikola83@gmail.com@87552490-4604-c2f1-3d08-01d065ce2e82>
2012-10-18 13:19:53 +0000

    Updated install SQL script, default data is inserted on install.

--- ccs/com_ccs/sql/install.mysql.utf8.sql
+++ ccs/com_ccs/sql/install.mysql.utf8.sql
@@ -13,7 +13,6 @@ CREATE TABLE IF NOT EXISTS `#__ccs_databases` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=27 ;
 
-
 --
 -- Table structure for table `ccs_ccs_admin_fields`
 --
@@ -27,3 +26,20 @@ CREATE TABLE IF NOT EXISTS `#__ccs_admin_fields` (
   `fld_params` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=45 ;
+
+INSERT INTO `#__ccs_admin_fields` (`id`, `ordering`, `db_alias`, `fld_alias`, `fld_type`, `fld_params`) VALUES
+(4, 21, 'ccs_databases', 'parent_db_alias', 'dbFieldList', 'ccs_databases,db_alias'),
+(6, 19, 'ccs_databases', 'db_alias', 'dbFieldList', ''),
+(7, 14, 'ccs_admin_fields', 'db_alias', 'dbFieldList', ''),
+(9, 18, 'ccs_databases', 'access', 'droplist', '1,Public\r\n2,Registered \r\n6,Manager\r\n7,Administrator\r\n8,Super User\r\n999,None'),
+(10, 20, 'ccs_databases', 'db_image', 'img', ''),
+(12, 12, 'ccs_admin_fields', 'fld_alias', '', ''),
+(13, 16, 'ccs_admin_fields', 'fld_params', 'plainText', ''),
+(14, 13, 'ccs_admin_fields', 'ordering', '', ''),
+(25, 15, 'ccs_admin_fields', 'fld_type', 'droplist', 'dbFieldList,dbFieldList\r\nfileList,fileList\r\nimg,img\r\ndroplist,droplist\r\ncheckbox,checkbox\r\nradio,radio\r\nbutton,button\r\nmodal,modal\r\nplainText,plainText\r\nrichText,richText\r\nlimitVarChar,limitVarChar\r\ndate,date\r\nboolean,boolean'),
+(26, 17, 'ccs_databases', 'ordering', '', ''),
+(31, 22, 'ccs_databases', 'quick_icon', 'boolean', '');
+
+INSERT INTO `#__ccs_databases` (`id`, `ordering`, `db_alias`, `access`, `db_image`, `parent_db_alias`, `quick_icon`) VALUES
+(23, 3, 'ccs_admin_fields', 8, 'administrator/components/com_ccs/assets/images/icon-ccs-48.png', '', 1),
+(24, 2, 'ccs_databases', 8, 'administrator/components/com_ccs/assets/images/icon-ccs-48.png', '', 1);
