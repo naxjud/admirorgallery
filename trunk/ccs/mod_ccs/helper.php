50b9be90a00d3a2906c0354b229cd0a83fa32750
Nikola83@gmail.com <Nikola83@gmail.com@87552490-4604-c2f1-3d08-01d065ce2e82>
2012-10-19 13:34:36 +0000

    Code refactoring, Code formating

--- trunk/ccs/mod_ccs/helper.php
+++ trunk/ccs/mod_ccs/helper.php
@@ -1,121 +1,110 @@
 <?php
+
 /**
  * @package		Joomla.Site
  * @subpackage	mod_breadcrumbs
  * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 // no direct access
 defined('_JEXEC') or die;
 
-class CCS
-{
+class CCS {
 
-    protected $CCS_Fields_Config;// List of Databases Identifier
-	protected $dbConfig;
-    protected $tablePrefix;
-    protected $tableAlias;
-    protected $templatesFolder;
-    protected $fields; 
-
-	public $moduleID;
-	public $tableID;
-	public $tmpTable;
-	public $tmpRow;
-	public $rowID;
-    public $templatePath;  
+    private $ccs_fields_config; // List of Databases Identifier
+    private $dbConfig;
+    private $dbObject;
+    private $tableAlias;
+    private $templatesFolder;
+    private $fields;
+    public $moduleID;
+    public $tableID;
+    public $tmpTable;
+    public $tmpRow;
+    public $rowID;
+    public $templatePath;
     public $output;
-	public $layout;
+    public $layout;
 
-    function __construct($moduleID,$tableID,$tmpTable,$tmpRow)
-	{
-		$conf = JFactory::getConfig();
-		$this->CCS_Fields_Config="ccs_admin_fields";
-		$this->templatesFolder = JPATH_ROOT.DS.'modules'.DS.'mod_ccs'.DS.'templates'.DS;
-		$this->dbConfig    = 'ccs_databases';		
-		$this->moduleID    = $moduleID;			
-		$this->tableID     = $tableID;		
-		$this->tmpTable    = $tmpTable;		
-		$this->tmpRow      = $tmpRow;
-	    $this->layout      = JRequest::getVar( 'ccs_layout_'.$moduleID,'table' );
-		$this->rowID       = JRequest::getVar( 'ccs_rowID_'.$moduleID,'0' );
-	    $this->tablePrefix = $conf->get('dbprefix');
-	    $this->tableAlias  = $this->getTableAlias();	
-	    $this->fields      = $this->loadVisibleFields();	
-	    if($this->layout=="table"){
-	    	$this->getTable();
-	    }else{
-	    	$this->getRow();
-	    }
-	}
+    function __construct($moduleID, $tableID, $tmpTable, $tmpRow) {
+        $this->CCS_Fields_Config = "ccs_admin_fields";
+        $this->templatesFolder = JPATH_ROOT . DS . 'modules' . DS . 'mod_ccs' . DS . 'templates' . DS;
+        $this->dbConfig = 'ccs_databases';
+        $this->dbObject = JFactory::getDBO();
+        $this->moduleID = $moduleID;
+        $this->tableID = $tableID;
+        $this->tmpTable = $tmpTable;
+        $this->tmpRow = $tmpRow;
+        $this->layout = JRequest::getVar('ccs_layout_' . $moduleID, 'table');
+        $this->rowID = JRequest::getVar('ccs_rowID_' . $moduleID, '0');
+        $this->tableAlias = $this->getTableAlias();
+        $this->fields = $this->loadVisibleFields();
+        if ($this->layout == "table") {
+            $this->getTable();
+        } else {
+            $this->getRow();
+        }
+    }
 
-	protected function loadVisibleFields(){
-				
-		$db = JFactory::getDBO();
-		$query = $db->getQuery(true);
-		$query->select($db->nameQuote('fld_alias'));
-		$query->select($db->nameQuote('fld_type'));
-		$query->select($db->nameQuote('fld_params'));
-		$query->from($db->nameQuote($this->tablePrefix.$this->CCS_Fields_Config));
-		$query->where($db->nameQuote('db_alias').' = '.$db->Quote($this->tableAlias));
-		$query->order($db->getEscaped('ordering'.' '.'ASC'));
-		$db->setQuery($query);
-		$fieldsArray = $db->loadAssocList();
-		$idArray = Array();
-		$idArray['fld_alias']="id";
-		$idArray['fld_type']="number";
-		$idArray['fld_params']="";	
-		array_unshift($fieldsArray, $idArray);// <-------------- Always insert id
-		return $fieldsArray;
+    protected function loadVisibleFields() {
 
-	}
+        $query = $this->dbObject->getQuery(true);
+        $query->select($this->dbObject->nameQuote('fld_alias'));
+        $query->select($this->dbObject->nameQuote('fld_type'));
+        $query->select($this->dbObject->nameQuote('fld_params'));
+        $query->from($this->dbObject->nameQuote('#__' . $this->ccs_fields_config));
+        $query->where($this->dbObject->nameQuote('db_alias') . ' = ' . $this->dbObject->Quote($this->tableAlias));
+        $query->order($this->dbObject->getEscaped('ordering' . ' ' . 'ASC'));
+        $this->dbObject->setQuery($query);
+        $fieldsArray = $this->dbObject->loadAssocList();
+        $idArray = Array();
+        $idArray['fld_alias'] = "id";
+        $idArray['fld_type'] = "number";
+        $idArray['fld_params'] = "";
+        array_unshift($fieldsArray, $idArray); // <-------------- Always insert id
+        return $fieldsArray;
+    }
 
-	protected function getTableAlias(){
-		$db = JFactory::getDBO();
-	    $query = $db->getQuery(true);
-	    $query->select($db->nameQuote('db_alias'));
-		$query->from($db->nameQuote($this->tablePrefix.$this->dbConfig));
-	    $query->where($db->nameQuote('id').'='.(int)$this->tableID); 
-	    $db->setQuery($query);
-	    return $db->loadResult();
-	}
+    protected function getTableAlias() {
+        $query = $this->dbObject->getQuery(true);
+        $query->select($this->dbObject->nameQuote('db_alias'));
+        $query->from($this->dbObject->nameQuote('#__' . $this->dbConfig));
+        $query->where($this->dbObject->nameQuote('id') . '=' . (int) $this->tableID);
+        $this->dbObject->setQuery($query);
+        return $this->dbObject->loadResult();
+    }
 
-	protected function getTable()
-	{
-		$db = JFactory::getDBO();
-	    $query = $db->getQuery(true);
-		foreach ($this->fields as $field) {
-			$query->select($db->nameQuote($field['fld_alias']));
-		}
-		$query->from($db->nameQuote($this->tablePrefix.$this->tableAlias));
-		$query->order($db->getEscaped('ordering'.' '.'ASC'));
-	    $db->setQuery($query);
-	    $this->output = $db->loadAssocList();
-	    $this->getTemplatePath();
-	}
+    protected function getTable() {
+        $query = $this->dbObject->getQuery(true);
+        foreach ($this->fields as $field) {
+            $query->select($this->dbObject->nameQuote($field['fld_alias']));
+        }
+        $query->from($this->dbObject->nameQuote('#__' . $this->tableAlias));
+        $query->order($this->dbObject->getEscaped('ordering' . ' ' . 'ASC'));
+        $this->dbObject->setQuery($query);
+        $this->output = $this->dbObject->loadAssocList();
+        $this->getTemplatePath();
+    }
 
-	protected function getRow()
-	{
-		$db = JFactory::getDBO();
-	    $query = $db->getQuery(true);
-		foreach ($this->fields as $field) {
-			$query->select($db->nameQuote($field['fld_alias']));
-		}
-		$query->from($db->nameQuote($this->tablePrefix.$this->tableAlias));
-	    $query->where($db->nameQuote('id').'='.(int)$this->rowID); 
-		$query->order($db->getEscaped('ordering'.' '.'ASC'));
-	    $db->setQuery($query);
-	    $this->output = $db->loadAssocList();
-	    $this->getTemplatePath();
-	}
+    protected function getRow() {
+        $query = $this->dbObject->getQuery(true);
+        foreach ($this->fields as $field) {
+            $query->select($this->dbObject->nameQuote($field['fld_alias']));
+        }
+        $query->from($this->dbObject->nameQuote('#__' . $this->tableAlias));
+        $query->where($this->dbObject->nameQuote('id') . '=' . (int) $this->rowID);
+        $query->order($this->dbObject->getEscaped('ordering' . ' ' . 'ASC'));
+        $this->dbObject->setQuery($query);
+        $this->output = $this->dbObject->loadAssocList();
+        $this->getTemplatePath();
+    }
 
-	protected function getTemplatePath()
-	{
-	    if($this->layout=="table"){
-	    	$this->templatePath = $this->templatesFolder.$this->tmpTable;
-	    }else{
-	    	$this->templatePath = $this->templatesFolder.$this->tmpRow;
-	    }
-	}
+    protected function getTemplatePath() {
+        if ($this->layout == "table") {
+            $this->templatePath = $this->templatesFolder . $this->tmpTable;
+        } else {
+            $this->templatePath = $this->templatesFolder . $this->tmpRow;
+        }
+    }
+
 }
--- trunk/ccs/mod_ccs/tmpl/default.php
+++ trunk/ccs/mod_ccs/tmpl/default.php
@@ -1,18 +1,17 @@
 <?php
- 
+
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 
 echo '
-<form action="'.JRoute::_('index.php').'" method="post" name="ccs_'.$CCS->moduleID.'" id="ccs_'.$CCS->moduleID.'">
+<form action="' . JRoute::_('index.php') . '" method="post" name="ccs_' . $CCS->moduleID . '" id="ccs_' . $CCS->moduleID . '">
 ';
 
-require($CCS->templatePath) ;
+require($CCS->templatePath);
 
 echo '
-<input type="hidden" name="ccs_layout_'.$CCS->moduleID.'" id="ccs_layout_'.$CCS->moduleID.'" value="'.$CCS->layout.'" />
-<input type="hidden" name="ccs_rowID_'.$CCS->moduleID.'" id="ccs_rowID_'.$CCS->moduleID.'" value="'.$CCS->rowID.'" />
+<input type="hidden" name="ccs_layout_' . $CCS->moduleID . '" id="ccs_layout_' . $CCS->moduleID . '" value="' . $CCS->layout . '" />
+<input type="hidden" name="ccs_rowID_' . $CCS->moduleID . '" id="ccs_rowID_' . $CCS->moduleID . '" value="' . $CCS->rowID . '" />
 </form>
 ';
-
 ?>
