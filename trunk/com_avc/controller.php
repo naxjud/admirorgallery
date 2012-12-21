7abc371db34ca81bc6ab8606a8f72a6ddb6b2c08
Nikola83@gmail.com <Nikola83@gmail.com@87552490-4604-c2f1-3d08-01d065ce2e82>
2012-12-21 13:56:32 +0000

    Initial release

---
+++ trunk/com_avc/controller.php
@@ -0,0 +1,23 @@
+<?php
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// import Joomla controller library
+jimport('joomla.application.component.controller');
+
+/**
+ * General Controller of CcsController component
+ */
+class AvcController extends JController {
+
+    /**
+     * Method to display the view
+     *
+     * @access    public
+     */
+    function display($cachable = false, $urlparams = false) {
+        parent::display();
+    }
+
+}
