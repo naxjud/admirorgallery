8a2741c0183eb53b44294ae3e28a02f6ef7b3336
Nikola83@gmail.com <Nikola83@gmail.com@87552490-4604-c2f1-3d08-01d065ce2e82>
2012-12-28 08:26:29 +0000

    Install SQL script updated to avc structure

--- com_avc/sql/uninstall.mysql.utf8.sql
+++ com_avc/sql/uninstall.mysql.utf8.sql
@@ -1,2 +1,3 @@
-DROP TABLE IF EXISTS `#__ccs_databases`;
-DROP TABLE IF EXISTS `#__ccs_admin_fields`;
+DROP TABLE IF EXISTS `#__avc_view_fields`;
+DROP TABLE IF EXISTS `#__avc_view`;
+DROP TABLE IF EXISTS `#__avc_field`;
