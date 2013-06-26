6d59141a0c08bd45cbd1118a80fefe1f4d6bc9b1
Igor.Kekeljevic@gmail.com <Igor.Kekeljevic@gmail.com@87552490-4604-c2f1-3d08-01d065ce2e82>
2013-06-26 13:18:39 +0000

    filelist and droplist updates language updates

--- com_avc/views/avc/tmpl/fld_types/row/droplist.php
+++ com_avc/views/avc/tmpl/fld_types/row/droplist.php
@@ -1,25 +1,27 @@
 <?php
 
-echo '<div class="form_items form_items1">';  
+echo '<div class="form_items form_items1">';
 
 // Create Form Field Label
 echo '<label id="jform_enabled-lbl" for="jform_enabled">';
 echo JText::_( strtoupper($FIELD_ALIAS));
 echo '</label>';
 
-echo '<select tabindex="'.$tabIndex.'" name="'.$FIELD_ALIAS.'" class="required editlinktip" title="'.JText::_('COM_CCS_TOOLTIPS_DROPLIST').'">';			
-echo '<option value="">'.JText::_("COM_CCS_SELECT_NONE").'</option>';
-$params_array = explode("\n",$FIELD_PARAMS);
-foreach($params_array as $params)// Add Dropbox item for any param founded
+echo '<select tabindex="'.$TABINDEX.'" name="'.$FIELD_ALIAS.'" class="required editlinktip" title="'.JText::_('COM_AVC_TOOLTIPS_DROPLIST').'">';			
+echo '<option value="">'.JText::_("COM_AVC_SELECT_NONE").'</option>';
+
+foreach($FIELD_PARAMS as $PARAM_KEY => $PARAM_VALUE)// Add Dropbox item for any param founded
 {
-	$value__label = explode(",",$params);
+
 	$selected=NULL;					
-	if($value__label[0]==$field_value)// Add Selected Value
+	if($PARAM_KEY == $FIELD_VALUE)// Add Selected Value
 	{
 		$selected=' selected="selected"';
 	}
-echo '<option value="'.$value__label[0].'"'.$selected.'>'.JText::_($value__label[1]).'</option>';
+	echo '<option value="'.$PARAM_KEY.'"'.$selected.'>'.JText::_( $PARAM_VALUE ).'</option>';
+
 }
+
 echo '</select>';
 
 echo '</div>';
--- com_avc/views/avc/tmpl/fld_types/row/fileList.php
+++ com_avc/views/avc/tmpl/fld_types/row/fileList.php
@@ -1,24 +1,100 @@
 <?php
 
-echo '<div class="form_items form_items1">';
+if(!empty($FIELD_PARAMS["folder"])){
+	$COM_AVC_FOLDER = $FIELD_PARAMS["folder"];
+}else{
+	$COM_AVC_FOLDER = "";
+}
+
+echo '<div class="form_items form_items2">';
 
 // Create Form Field Label
 echo '<label id="jform_enabled-lbl" for="jform_enabled">';
 echo JText::_( strtoupper($FIELD_ALIAS));
 echo '</label>';
 
-$tmp_files = JFolder::files(JPATH_SITE.DS.$field_params);
-echo '<select tabindex="'.$tabIndex.'" name="'.$FIELD_ALIAS.'" class="required editlinktip" title="'.JText::_('COM_CCS_TOOLTIPS_DROPLIST').'">';			
-echo '<option value="">'.JText::_("COM_CCS_SELECT_NONE").'</option>';
-foreach($tmp_files as $tmp_file)// Add Dropbox item for any param founded
-{
-	$selected=NULL;					
-	if($tmp_file==$FIELD_VALUE)// Add Selected Value
-	{
-		$selected=' selected="selected"';
-	}
-echo '<option value="'.$tmp_file.'"'.$selected.'>'.$tmp_file.'</option>';
+
+echo '
+	<input
+		id="'.$FIELD_ALIAS.'"
+		tabindex="' . $TABINDEX . '"
+		type="text"
+		name="' . $FIELD_ALIAS . '"
+		value="' . $FIELD_VALUE . '"
+		class=""
+		title="' . JText::_('COM_AVC_TOOLTIPS_VARCHAR') . '"
+	/>
+';
+
+
+
+///////////////////////////////////////////////
+//	BUTTONS SELECT & CLEAR
+///////////////////////////////////////////////
+echo '
+	<input type="button" class="pointer width_auto" value="'.JText::_('COM_AVC_SELECT').'" onclick="
+		SqueezeBox.fromElement($(\'' . $FIELD_ALIAS . '_wrap\'), {
+			handler:\'clone\',
+			size: {
+				x: 400,
+				y: 400,
+			}
+		});
+		return false;
+	"
+	>			    
+	<input type="button" class="pointer width_auto" value="'.JText::_('COM_AVC_CLEAR').'"
+	onclick="
+		avc_filelist_update(\'' . $FIELD_ALIAS . '\', \'\', \'\');
+		return false;
+	"
+	>
+';
+
+//////////////////////////////////////////////
+// CREATE POPUP WINDOW
+//////////////////////////////////////////////
+echo '
+<div style="display:none;">
+<div id="' . $FIELD_ALIAS . '_wrap">
+<h1>' . JText::_( strtoupper($FIELD_TITLE)) . '</h1>
+
+<ul>
+';
+
+$COM_AVC_FOLDER_CONTENT = JFolder::files(JPATH_SITE.DS.$COM_AVC_FOLDER);
+foreach($COM_AVC_FOLDER_CONTENT as $COM_AVC_FILE){
+	echo '
+	<li>
+		<a
+			href="#"
+			onclick="
+				window.parent.avc_filelist_update(\'' . $FIELD_ALIAS . '\', \'' . $COM_AVC_FOLDER . '\', \'' . $COM_AVC_FILE . '\');
+				window.parent.SqueezeBox.close();
+				return false;
+			"
+		>
+		' . $COM_AVC_FILE . '
+		</a>
+	</li>
+	';
 }
-echo '</select>';
+
+echo '
+</ul>
+</div>
+</div>
+';
+
+
+
+
+
+
+
+
+
+
+
 
 echo '</div>';
