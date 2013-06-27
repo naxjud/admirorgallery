2ce39ad4baafd92b03b38e4d9e601c88c41cfc8d
Nikola83@gmail.com <Nikola83@gmail.com@87552490-4604-c2f1-3d08-01d065ce2e82>
2013-06-27 14:26:33 +0000

    Pirobox Extended added

---
+++ com_admirorgallery_4/com_admirorgallery/admirorgallery/admirorgallery/popups/piroboxExtended/js/piroboxInit.js
@@ -0,0 +1,17 @@
+ /*------------------------------------------------------------------------
+# plg_admirorgallery - Admiror Gallery Plugin
+# ------------------------------------------------------------------------
+# author   Igor Kekeljevic & Nikola Vasiljevski
+# copyright Copyright (C) 2011 admiror-design-studio.com. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://www.admiror-design-studio.com/joomla-extensions
+# Technical Support:  Forum - http://www.vasiljevski.com/forum/index.php
+# Version: 4.1.1
+-------------------------------------------------------------------------*/
+AG_jQuery(document).ready(function(){
+	AG_jQuery().piroBox_ext({
+		  piro_speed : 400, //animation speed
+		  bg_alpha: 0.1, //background opacity
+		  piro_scroll : true //pirobox always positioned at the center of the page
+		});
+	});
